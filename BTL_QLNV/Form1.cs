ffe8d8991cb2c31d68a6f157012503086c98938a
dnaagkhoa <dnagkhoa2266@gmail.com>
2024-05-22 20:53:40 +0700

    Load Du Lieu len Textbox

--- BTL_QLNV/Form1.cs
+++ BTL_QLNV/Form1.cs
@@ -16,7 +16,7 @@ namespace BTL_QLNV
 
         SqlConnection connection;
         SqlCommand command;
-        string str = "Data Source=LAPTOP-3CLN13P2;Initial Catalog=QLCongTy;Integrated Security=True";
+        string str = "Data Source=Admin-PC;Initial Catalog=QLCongTy;Integrated Security=True";
         SqlDataAdapter adapter = new SqlDataAdapter();
         DataTable table1 = new DataTable();
         DataTable table2 = new DataTable();
@@ -51,7 +51,15 @@ namespace BTL_QLNV
 
         private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            int i;
+            i = dgv1.CurrentRow.Index;
+            tb_manv.Text = dgv1.Rows[i].Cells[0].Value.ToString();
+            tb_tennv.Text = dgv1.Rows[i].Cells[1].Value.ToString();
+            dtime_ngaysinh.Text = dgv1.Rows[i].Cells[2].Value.ToString();
+            cb_gioitinh.Text = dgv1.Rows[i].Cells[3].Value.ToString();
+            tb_chucvu.Text = dgv1.Rows[i].Cells[4].Value.ToString();
+            tb_tienluong.Text = dgv1.Rows[i].Cells[5].Value.ToString();
+            tb_mapb1.Text = dgv1.Rows[i].Cells[6].Value.ToString();
         }
 
         private void dgv2_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -66,5 +74,6 @@ namespace BTL_QLNV
             loaddata1();
             loaddata2();
         }
+
     }
 }
