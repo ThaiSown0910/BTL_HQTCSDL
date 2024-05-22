cd9dd35d72403cdf1e5905aa1a746b610b320fbc
Thai Son <thaison09102000@gmail.com>
2024-05-22 20:29:42 +0700

    Load Du lieu va connect DataBase

--- BTL_QLNV/Form1.cs
+++ BTL_QLNV/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,58 @@ namespace BTL_QLNV
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection connection;
+        SqlCommand command;
+        string str = "Data Source=LAPTOP-3CLN13P2;Initial Catalog=QLCongTy;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table1 = new DataTable();
+        DataTable table2 = new DataTable();
+
+        void loaddata1()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select * from ThongTinNhanVien";
+            adapter.SelectCommand = command;
+            table1.Clear();
+            adapter.Fill(table1);
+            dgv1.DataSource = table1;
+        }
+
+        void loaddata2()
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "select * from ThongTinPhongBan";
+            adapter.SelectCommand = command;
+            table2.Clear();
+            adapter.Fill(table2);
+            dgv2.DataSource = table2;
+        }
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgv2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(str);
+            connection.Open();
+            loaddata1();
+            loaddata2();
         }
     }
 }
